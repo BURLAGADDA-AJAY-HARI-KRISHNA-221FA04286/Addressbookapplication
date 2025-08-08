a0d22c9616464301a99a669e4e730983f8cf22bb
BURLAGADDA AJAY HARI KRISHNA <ajayburlagadda@gmail.com>
2025-08-08 22:53:00 +0530

    version 2

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import Controller.AddressBookController;
+
+public class Main {
+    public static void main(String[] args) {
+        AddressBookController controller = new AddressBookController();
+        controller.run();
+    }
+}
